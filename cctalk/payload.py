d13cb23da880d215b10196654aa84f05db2ccc98
TS <tomichec@gmail.com>
2019-07-31 12:48:27 +0200

    updates the cctalk, adds the crc-ccit function for 16 bit checksum

--- cctalk/payload.py
+++ cctalk/payload.py
@@ -1,23 +1,50 @@
 from headers import H
 
-def check(sum):
-    return (256 - sum) % 256
-
-def msg(payload):
+def simpleChsum(payload):
     sum = 0
     for i in payload:
-        print("%02x" %(i), end=' ')
         sum += i
-    print("%02x" %(check(sum)))
 
+    checksum = -sum % 0x100
+    print(FMT % checksum)
+
+def crc16ccitt(crc, data):
+    # taken from:
+    # https://stackoverflow.com/questions/25239423/crc-ccitt-16-bit-python-manual-calculation
+    # consistent with results from:
+    # https://www.lammertbies.nl/comm/info/crc-calculation.html
+    msb = crc >> 8
+    lsb = crc & 255
+    for c in data:
+        x = c ^ msb
+        x ^= (x >> 4)
+        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
+        lsb = (x ^ (x << 5)) & 255
+    checksum = (msb << 8) + lsb
+    
+    print(FMT % (checksum))
+
+def msg(payload):
+    check = simpleChsum
+    for i in payload:
+        print(FMT %(i), end=' ')
 
 def cctForm(dest, cmd, data=[], source = 1):
-
-    payload = [dest, len(data), source, cmd, *data]
-    msg(payload)
+    return [dest, len(data), source, cmd, *data]
 
 def main():
-    cctForm(0,H['simple_poll'], source = 0x01)
+    check = simpleChsum
+    payload = cctForm(2,H['simple_poll'], source = 0x01)
+    # payload = cctForm(2,242, source = 0x01)
+    # payload= [76, 97, 109, 109, 101, 114, 116]
+    msg(payload)
+    simpleChsum(payload)
+
+    # crc = 0x1021
+    # crc = 0x1d0f
+    # crc16ccitt(crc, payload)
 
 if (__name__ == "__main__"):
+    FMT="%02x"
+    # FMT="%d"
     main()
