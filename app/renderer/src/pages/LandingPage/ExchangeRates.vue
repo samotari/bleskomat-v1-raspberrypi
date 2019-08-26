5d5a27d6f566e54505465b33dda060152357ef96
Robin Cussol <me@robincussol.com>
2019-08-26 19:46:43 +0200

    feat: basic landing page
    The exchange rates section is mocked for now, will later use data made available by https://github.com/samotari/bleskomat/pull/37.
    
    Some basic design choices were made; we might need to consider some kind of design system to have consistent UI.
    
    I also added some basic (and partial) hot reloading to improve DX.

---
+++ app/renderer/src/pages/LandingPage/ExchangeRates.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<main>
+			<ul>
+				<li>1 BTC = 9,600 EUR</li>
+				<li>1 BTC = 200,000 CZK</li>
+			</ul>
+		</main>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ExchangeRates',
+};
+</script>
+
+<style scoped>
+ul {
+	text-align: left;
+	font-weight: bold;
+	list-style-type: none;
+}
+</style>
---
+++ app/renderer/src/pages/LandingPage/LandingPage.vue
@@ -0,0 +1,47 @@
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+}
+
+.wrapper {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+}
+</style>
+
+<template>
+	<div class="wrapper">
+		<header>
+			<img src="../../assets/bitcoin-lightning.svg" :style="styleObject" />
+			<ExchangeRates />
+		</header>
+		<main>
+			<div class="actions">
+				<Button label="Get Started" @on-click="start()" />
+			</div>
+		</main>
+		<footer>Bleskomat, <em>Lightning Network ATM</em></footer>
+	</div>
+</template>
+
+<script>
+import Button from '../../components/Button';
+import ExchangeRates from './ExchangeRates';
+
+export default {
+	name: 'LandingPage',
+	components: { Button, ExchangeRates },
+	data: () => ({
+		styleObject: {
+			width: '10%',
+		},
+	}),
+	methods: {
+		start() {
+			this.$router.push('/scan-invoice');
+		},
+	},
+};
+</script>
